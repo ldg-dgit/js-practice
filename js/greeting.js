bf4fa312117266ffe17c69bf37e25dc09e0bca9b
Bobby Lee <ldg@dgit.co>
2022-06-15 15:14:15 +0900

    Finish Clock Screen

--- js/greeting.js
+++ js/greeting.js
@@ -8,19 +8,21 @@ const KEY_USERNAME = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(CLASSNAME_HIDDEN);
-  localStorage.setItem(KEY_USERNAME, loginInput.value);
-  paintGreetings();
+  const usernameFromInput = loginInput.value;
+  localStorage.setItem(KEY_USERNAME, usernameFromInput);
+  paintGreetings(usernameFromInput);
 }
 
-function paintGreetings() {
-  const username = localStorage.getItem(KEY_USERNAME);
+function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(CLASSNAME_HIDDEN);
 }
 
-if (savedUsername === null) {
+const usernameFromLocal = localStorage.getItem(KEY_USERNAME);
+
+if (usernameFromLocal === null) {
   loginForm.classList.remove(CLASSNAME_HIDDEN);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings();
+  paintGreetings(usernameFromLocal);
 }
